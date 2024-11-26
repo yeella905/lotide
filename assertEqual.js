73ac0e8dd3d6b14310ac953e716ecbf8006cb794
yeella905 <yeella905@gmail.com>
2024-11-26 12:42:09 -0500

    made a personal assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
