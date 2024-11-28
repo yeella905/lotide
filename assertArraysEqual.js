6a2325918f50f10384cc04a2bbe282d8a39bbe02
yeella905 <yeella905@gmail.com>
2024-11-28 11:18:21 -0500

    added assertArraysEqual mod

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,34 @@
-const eqArrays = (array1, array2) => {
-  
-  if (array1.length !== array2.length) {
-    return `this will be ${false}.`;
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴Assertion failed: ${actual} !== ${expected}`);
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return `this will be ${false}.`;
-    }
-  }
-  return `this will be ${true}.`;
   
 };
   
-//   eqArrays([1, 2, 3], [1, 2, 3]), true; // => should PASS
-  
-  
+const eqArrays = (array1, array2) => {
 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => True
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const assertArraysEqual = (firstArray, secArray) => {
+  if (eqArrays(firstArray,secArray)) {
+    console.log("🟢Assertion passed: true");
+  } else {
+    console.log("🔴Assertion failed: false");
+  }
+  
+};
+
+return assertArraysEqual();
