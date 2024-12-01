b080316fb28576dd5c25863dc752867a96328e1b
yeella905 <yeella905@gmail.com>
2024-12-01 02:46:07 -0500

    countLetter

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = (actual, expected) => {
+    if (actual === expected) {
+      console.log(`🟢Assertion passed: ${actual} === ${expected}`);
+    } else if (actual !== expected) {
+      console.log(`🔴Assertion failed: ${actual} !== ${expected}`);
+    }
+  
+  };
+
+  const countLetters = (allLetters) => {
+
+    const forResults = {}
+    for (const letter of allLetters) {
+        if (letter in forResults) {
+            forResults[letter] += 1 
+          } else {
+            forResults[letter] = 1
+          }
+        }
+        //return result
+    return forResults;
+    };
+
+
+console.log(countLetters("ella"));
+
+
+
+    // if (result[letter]) {
+    //     result[letter] += 1;
+    //   } else {
+    //     result[letter] = 1;
+    //   }
--- countOnly.js
+++ countOnly.js
@@ -15,7 +15,6 @@ const countOnly = function (allItems, itemsToCount) {
     const results = {};
   
     for (const item of allItems) {
-      console.log(item);
       if (itemsToCount[item]) {
         if (results[item]) {
           results[item] += 1;
