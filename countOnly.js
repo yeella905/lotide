ddefeb81342334c2feb46c9d9a18c2ef84b5b79f
yeella905 <yeella905@gmail.com>
2024-12-01 00:00:08 -0500

    add reults to count

--- countOnly.js
+++ countOnly.js
@@ -16,6 +16,11 @@ const countOnly = function (allItems, itemsToCount) {
   
     for (const item of allItems) {
       console.log(item);
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   
     return results;
