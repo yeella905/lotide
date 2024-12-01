edba9118ead7f0f17fe01e14d2d2e48f197fafdf
yeella905 <yeella905@gmail.com>
2024-11-30 23:52:33 -0500

    count only

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = (actual, expected) => {
+    if (actual === expected) {
+      console.log(`🟢Assertion passed: ${actual} === ${expected}`);
+    } else if (actual !== expected) {
+      console.log(`🔴Assertion failed: ${actual} !== ${expected}`);
+    }
+  
+  };
+  
+//   // TEST CODE
+//   assertEqual("Lighthouse Labs", "Bootcamp");
+//   assertEqual(1, 1);
+
+const countOnly = function (allItems, itemsToCount) {};
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe",
+  ];
+  
+  const result1 = countOnly(firstNames, {
+    Jason: true,
+    Karima: true,
+    Fang: true,
+    Agouhanna: false,
+  });
+  
+ 
+
+  console.log(assertEqual(result1["Jason"], 1));
+  console.log(assertEqual(result1["Karima"], undefined));
+  console.log(assertEqual(result1["Fang"], 2));
+  console.log(assertEqual(result1["Agouhanna"], undefined));
+
+const results = {};
+return results;
