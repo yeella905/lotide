b1eb4052d33cbe2aa96a65ee724567494fac920e
yeella905 <yeella905@gmail.com>
2024-12-03 01:16:07 -0500

    adding map.js

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+
+const map = function () {
+    const results = [];
+    return results;
+  };
+
