785f11474583100e5163860bb8ceb18eff13aa48
yeella905 <yeella905@gmail.com>
2024-12-12 03:30:24 -0500

    refract v14

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,9 @@
-const assertEqual = require("../assertEqual");
+const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual(middle([1])); // => []
+assertArraysEqual(middle([1, 2])); // => []
+assertArraysEqual(middle([1, 2, 3])); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5])); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4])); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
