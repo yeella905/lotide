2ae9dd69f3a4a8edb0085146aff3fd8f5d2e4782
yeella905 <yeella905@gmail.com>
2024-12-12 03:24:54 -0500

    refract v13

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = (array1, array2) => {
-  
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-    
-};
-    
-const assertArraysEqual = (firstArray, secArray) => {
-  if (eqArrays(firstArray,secArray)) {
-    console.log(`🟢Assertion passed: ${firstArray} === ${secArray}`);
-  } else {
-    console.log(`🔴Assertion failed: ${firstArray} !== ${secArray}`);
-  }
-      
-};
-
-
 const middle = (array) => {
   const result = [];
   if (array.length <= 2) {
@@ -37,10 +13,4 @@ const middle = (array) => {
   return result;
 };
 
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
