d2cdec91e177bfebc3fcb4a76a39995a5b009974
yeella905 <yeella905@gmail.com>
2024-12-12 15:19:59 -0500

    mac v6

--- test/middleTest.js
+++ test/middleTest.js
@@ -24,4 +24,7 @@ it("returns [3] for [1, 2, 3, 4, 5]", () => {
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
+  it ("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
 });
