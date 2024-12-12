2734ea325b215e46bf73afc4aed639f7dc8c8298
yeella905 <yeella905@gmail.com>
2024-12-12 15:18:13 -0500

    mac v5

--- test/middleTest.js
+++ test/middleTest.js
@@ -18,4 +18,10 @@ it("make sure the original array was not altered by the middle function", () => 
     middle(words);
     assert.strictEqual(words.length, 3);
 });
+it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
 });
